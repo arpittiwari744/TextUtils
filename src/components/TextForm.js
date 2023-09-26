b6eeb49aae0b65afea30ea4d82847e036b8141be
Arpit <arpittiwari0744gmail.com>
2023-09-26 21:18:07 +0530

    Initial commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -66,19 +66,19 @@ return (
           <textarea className="form-control" value={text} id="mytext" rows="8" placeholder='Enter text here' onChange={handleonchange} style={{ backgroundColor: props.mode === `dark` ? `#042743` : `white` }}></textarea>
         </div>
       </form>
-      <button className="btn btn-primary mx-1" type="submit" onClick={handleupclick} >Change to Uppercase</button>
-      <button className="btn btn-primary mx-1" type="submit" onClick={handleloclick} >Change to Lowercase</button>
-      <button className="btn btn-primary mx-1" type="submit" onClick={handlectclick} >Copy Text</button>
-      <button className="btn btn-primary mx-1" type="submit" onClick={handleclearclick} >Clear</button>
-      <button className="btn btn-primary mx-1" type="submit" onClick={handleexclick} >Extra Space Remover</button>
-      <button className="btn btn-primary mx-1" type="submit" onClick={handlelrclick} >New Line remover</button>
-      <button className="btn btn-primary mx-1" type="submit" onClick={handlerpclick} >Remove Punctuations</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" type="submit" onClick={handleupclick} >Change to Uppercase</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" type="submit" onClick={handleloclick} >Change to Lowercase</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" type="submit" onClick={handlectclick} >Copy Text</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" type="submit" onClick={handleclearclick} >Clear</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" type="submit" onClick={handleexclick} >Extra Space Remover</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" type="submit" onClick={handlelrclick} >New Line remover</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" type="submit" onClick={handlerpclick} >Remove Punctuations</button>
     </div>
     <div className="container my-3" style={{ color: props.mode === `dark` ? `white` : `#042743` }}>
       <h2>Your text summary</h2>
-      <p>{(text.split(" ")).filter(word => word !== '').length} words and {text.length} characters</p>
+      <p>{(text.split(/\s+/)).filter(word => word !== '').length} words and {text.length} characters</p>
       <h2>Preview</h2>
-      <p>{text.length > 0 ? text : "Enter your text above to preview it here"}</p>
+      <p>{text.length > 0 ? text : "Nothing to preview!"}</p>
     </div>
   </>
 )
